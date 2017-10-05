5c8958803a73303b9f6b8178026e023db0786c5c
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-04 23:55:54 -0500

    Added dotenv call

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// for .env vars
+require('dotenv').config()
 // enable @std/esm loader
 require = require('@std/esm')(module, { cjs: true, esm: "js" })
 module.exports = require('./src/index.js').default
