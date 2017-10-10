3b47826efb81179f817538958d34de410042bfb1
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-10 13:51:29 -0500

    Added get query templates/examples.

--- src/db/polls.js
+++ src/db/polls.js
@@ -1,2 +1,50 @@
 import db from 'db-instance'
 
+/**
+ * /polls
+ * -> [{poll: 'Name of poll'}...]
+ * 
+ * /polls/:poll_id
+ * -> {
+ *   poll: 'Name of poll',
+ *   options: [
+ *     {
+ *       name: 'Option 1',
+ *       votes: 2,
+ *     },
+ *     {...}
+ *   ],
+ *   usersVoted: ['steve123',...],
+ *   ipAddressesVoted: ['127.0.0.1',...]
+ * }
+ *
+ */
+
+const getAllPolls = async () => {
+
+    /**
+     * example query:
+     * -> SELECT poll_id,title FROM polls;
+     *
+     */
+
+}
+
+const getPoll = async poll_id => {
+
+    /**
+     * example query:
+     * -> SELECT polls.title, options.name, COUNT(votes.vote) AS num_of_votes, votes.users, votes.ip_addresses
+     *    FROM votes 
+     *    INNER JOIN options ON options.option_id = votes.options_id
+     *    INNER JOIN polls ON options.poll_id = polls.poll_id
+     *    GROUP BY options.name;
+     *
+     */
+
+}
+
+export {
+    getAllPolls,
+    getPoll
+}
