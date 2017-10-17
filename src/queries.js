4a7cd2284c5492865293783bc90317535afb181e
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-17 12:02:13 -0500

    Added clearer query examples

--- src/queries.js
+++ src/queries.js
@@ -25,26 +25,43 @@ const db = pg()(process.env.DATABASE_URL)
 const getAllPolls = async () => {
     /**
      * possible query:
-     * -> SELECT poll_id,title FROM polls;
+     * -> SELECT poll_id, title FROM polls;
+     *
+     *     poll_id |             title
+     *    ---------+-------------------------------
+     *           1 | What is your favorite color?
+     *           2 | What is your favorite animal?
      */
 }
 
 const getUserPolls = async user_id => {
     /**
      * possible query:
-     * -> SELECT title
-     *    FROM polls
-     *    WHERE user_id = $user_id
+     * -> SELECT poll_id, title
+     *    FROM polls 
+     *    WHERE user_id = <user_id>;
+     *
+     *     poll_id |            title
+     *    ---------+------------------------------
+     *           1 | What is your favorite color?
      */
 }
 
 const getPoll = async poll_id => {
     /**
      * possible query:
-     * -> SELECT options.name, COUNT(votes.vote) AS num_of_votes, votes.users, votes.ip_addresses
-     *    FROM votes 
-     *    INNER JOIN options ON options.option_id = votes.options_id
-     *    GROUP BY options.name;
+     * -> SELECT polls.title, options.option, COUNT(votes.vote_id) as num_of_votes
+     *    FROM votes
+     *    INNER JOIN options ON options.option_id = votes.option_id
+     *    INNER JOIN polls ON options.poll_id = polls.poll_id
+     *    WHERE poll_id = <poll_id>
+     *    GROUP BY polls.poll_id, options.option;
+     *
+     *                title             | option | num_of_votes
+     *    ------------------------------+--------+--------------
+     *     What is your favorite color? | green  |            2
+     *     What is your favorite color? | red    |            1
+     *     What is your favorite color? | blue   |            1
      */
 }
 
@@ -52,7 +69,7 @@ const addUser = async username => {
     /**
      * possible query:
      * -> INSERT INTO users(username)
-     *    VALUES($username);
+     *    VALUES(<username>);
      */
 }
 
@@ -60,26 +77,27 @@ const addPoll = async (user_id, title, options) => {
     /**
      * possible query:
      * -> INSERT INTO polls(title, user_id)
-     *    VALUES('Title', 1);
+     *    VALUES(<title>, <user_id>);
      *
-     *    INSERT INTO options(name)
-     *    VALUES('option1');
+     *    (for each <option> in <options>)
+     *    INSERT INTO options(option, poll_id)
+     *    VALUES(<option>, (SELECT poll_id FROM polls WHERE title = <title>));
      */
 }
 
-const addOption = async (name, poll_id) {
+const addOption = async (option, poll_id) {
     /**
      * possible query:
-     * -> INSERT INTO options(name, poll_id)
-     *    VALUES('option2', 1);
+     * -> INSERT INTO options(option, poll_id)
+     *    VALUES(<option>, <poll_id>);
      */
 }
 
-const addVote = async (poll_id, option_id, user_id, ip_address) {
+const addVote = async (poll_id, option_id, username_or_ip) {
     /**
      * possible query:
-     * -> INSERT INTO votes(poll_id, option_id, user_id, ip_address)
-     *    VALUES(1,1,'127.0.0.1');
+     * -> INSERT INTO votes(poll_id, option_id, username_or_ip)
+     *    VALUES(<poll_id>, <option_id>, <username_or_ip>);
      */
 }
 
