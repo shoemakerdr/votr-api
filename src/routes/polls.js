6c61be7506618bd8d08e273d56805fcafe3f1984
Derek Shoemaker <shoemakerdr@gmail.com>
2017-10-20 22:20:43 -0500

    Added post routes to /polls, /polls/:poll, /polls/:poll/option, and delete route to /polls/:poll

--- src/routes/polls.js
+++ src/routes/polls.js
@@ -1,5 +1,12 @@
 import { Router } from 'express'
-import { getAllPolls, getPoll } from '../queries'
+import {
+    getAllPolls,
+    getPoll,
+    addPoll,
+    addOption,
+    addVote,
+    removePoll
+} from '../queries'
 
 const router = Router()
 
@@ -7,10 +14,31 @@ router.route('/')
     .get((req, res) => {
         getAllPolls().then(data => res.json(data))
     })
+    .post((req,res) => {
+        const userId = req.body.userId
+        const title = req.body.title
+        const options = req.body.options.split(',')
+        addPoll(userId, title, options).then(data => res.json(data))
+    })
 
 
 router.route('/:poll')
     .get((req, res) => {
+        getPoll(req.params.poll).then(data => res.json(data))
+    })
+    .delete((req, res) => {
+        removePoll(req.params.poll).then(data => res.json(data))
+    })
+
+router.route('/:poll/option')
+    .post((req, res) => {
+        const option = req.body.option
+        const pollId = req.params.poll
+        addOption(option, pollId).then(data => res.json(data))
+    })
+
+router.route('/:poll/vote')
+    .post((req, res) => {
         getPoll(req.params.poll).then(data => res.json(data))
     })
 
