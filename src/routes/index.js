f6e30b636175f965e723e1d5bda2fb0110449c4f
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-12 22:17:12 -0600

    Added jwt auth middleware for /polls and /users routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import polls from './polls'
 import users from './users'
 import auth from './auth'
+import jwt from 'jsonwebtoken'
 
 const router = Router()
 
@@ -11,8 +12,20 @@ router.route('/')
         res.json(json)
     })
 
-router.use('/polls', polls)
-router.use('/users', users)
+// JWT auth middleware
+const jwtAuth = (req, res, next) => {
+    const token = req.headers['token']
+    if (token)
+        jwt.verify(token, process.env.AUTH_SECRET, (err, decode) => {
+            if (err)
+                res.status(500).json({error: 'Invalid token'})
+            else next()
+        })
+    else res.json({error: 'Must include token with requests'})
+}
+
+router.use('/polls', jwtAuth, polls)
+router.use('/users', jwtAuth, users)
 router.use('/auth', auth)
 
 export default router
