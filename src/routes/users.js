e6121146cef9659b88364d2851a0c0aadad103d1
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-13 00:33:09 -0600

    Imported jwtAuth middleware for /users route.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { getUserPolls, createUser } from '../queries'
+import { jwtAuth } from '../helpers'
 
 const router = Router()
 
 router.route('/:username/polls')
-    .get((req, res) => {
+    .get(jwtAuth, (req, res) => {
         getUserPolls(req.params.username).then(data => res.json(data))
     })
 
