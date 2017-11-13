a1d034ae28cfba0bc24a4b20c6150c70a2fdfcd9
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-12 22:17:49 -0600

    Removed route handlers for /users and /users/:username

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,19 +3,6 @@ import { getUserPolls, createUser } from '../queries'
 
 const router = Router()
 
-router.route('/')
-    .post((req, res) => {
-        const username = req.body.username
-        const password = req.body.password
-        createUser(username, password).then(data => res.json(data))
-    })
-
-router.route('/:username')
-    .get((req, res) => {
-        const json = {username:req.params.username}
-        res.json(json)
-    })
-
 router.route('/:username/polls')
     .get((req, res) => {
         getUserPolls(req.params.username).then(data => res.json(data))
