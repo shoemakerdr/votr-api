c3f8679b65a85bc6e563928c7c713a10eb86832c
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-12 22:16:36 -0600

    Added jwt and passport middleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,30 +3,50 @@ import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
 import { createUser, getUser } from '../queries'
 import { comparePassword, createPasswordHash } from '../helpers'
+import jwt from 'jsonwebtoken'
 
 //configure passport strategy
-// passport.use(new LocalStrategy(
-    // (username, password, done) => {
-//
-    // }
-// ))
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+            const user = await getUser(username)
+            if (user.error)
+                return done(null, false)
+            if (!comparePassword(password, user.password))
+                return done(null, false)
+            return done(null, user)
+        }
+        catch (err) {
+            return done(err)
+        }
+    }
+))
+
+// jwt token generation
+ const generateToken = username => (req, res) => {
+    const expiration = Math.floor(Date.now() / 1000) + 3600
+    const token = jwt.sign({user: username}, process.env.AUTH_SECRET, {
+        expiresIn: 3600
+    })
+    res.json({
+            token: token,
+            username: username,
+            expiration: expiration
+    })
+}
+
 const router = Router()
 
 router.route('/login')
-    .post((req, res) => {
-        console.log(req.body)
-        const username = req.body.username
-        const password = req.body.password
-        console.log(username)
-        getUser(username).then(data => res.json(data))
-    })
+    .post(passport.authenticate('local', {session: false}), (req,res) => generateToken(req.user.username)(req,res))
 
 router.route('/register')
     .post((req, res) => {
         const username = req.body.username
         const password = createPasswordHash(req.body.password)
-        res.json({user: username, password: password})
-        // createUser(username, password).then(data => res.json(data))
+        // res.json({user: username, password: password})
+        createUser(username, password)
+            .then(data => generateToken(data.username)(req,res))
     })
 
 export default router
