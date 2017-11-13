65eaf21a0cbf4cd5390ec68030da27be2b0a9ee3
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-13 00:31:34 -0600

    Added generateToken and jwtAuth middleware functions.

--- src/helpers.js
+++ src/helpers.js
@@ -9,7 +9,32 @@ const createPasswordHash = password => {
     return bcrypt.hashSync(password, salt)
 }
 
+const generateToken = username => (req, res) => {
+    const expiration = Date.now() + 3600000
+    const token = jwt.sign({user: username}, process.env.AUTH_SECRET, {
+        expiresIn: 3600
+    })
+    res.json({
+        token: token,
+        username: username,
+        expiration: expiration
+    })
+}
+
+const jwtAuth = (req, res, next) => {
+    const token = req.headers['token']
+    if (token)
+        jwt.verify(token, process.env.AUTH_SECRET, (err, decode) => {
+            if (err)
+                res.status(500).json({error: 'Invalid token'})
+            else next()
+        })
+    else res.json({error: 'Must include token with requests'})
+}
+
 export {
     comparePassword,
     createPasswordHash,
+    generateToken,
+    jwtAuth,
 }
