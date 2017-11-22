1e38787ea1028401710dbbc58d92ffdc32bc168e
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-21 21:13:38 -0600

    Deleted unnecessary index file.

--- index.js
+++
@@ -1,5 +0,0 @@
-// for .env vars
-require('dotenv').config()
-// enable @std/esm loader
-require = require('@std/esm')(module, { cjs: true, esm: "js" })
-module.exports = require('./src/index.js').default
